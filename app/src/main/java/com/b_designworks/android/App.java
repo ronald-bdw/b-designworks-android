33c6f5ff694c499c8016f60c1ca41f991eb8714b
Илья Еремин <partissanei@gmail.com>
2016-08-02 14:43:31 +0300

    even more crashlytics tune up

--- app/src/main/java/com/b_designworks/android/App.java
+++ app/src/main/java/com/b_designworks/android/App.java
@@ -1,7 +1,9 @@
 package com.b_designworks.android;
 
 import android.app.Application;
+
 import com.crashlytics.android.Crashlytics;
+
 import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
@@ -9,8 +11,13 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        setUpCrashMonitoring();
+    }
+
+    private void setUpCrashMonitoring() {
         Fabric.with(this, new Crashlytics());
-        // your super code here
+        Crashlytics.setString("GIT_SHA", BuildConfig.GIT_SHA);
+        Crashlytics.setBool("DEBUG", BuildConfig.DEBUG);
     }
 
 }
