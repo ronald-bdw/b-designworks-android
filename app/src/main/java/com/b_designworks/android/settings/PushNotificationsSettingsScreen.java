ac5103fab0ca80c7c0472697423909514bd0b38c
Илья Еремин <partissanei@gmail.com>
2016-08-09 13:36:22 +0300

    push notification screen

---
+++ app/src/main/java/com/b_designworks/android/settings/PushNotificationsSettingsScreen.java
@@ -0,0 +1,20 @@
+package com.b_designworks.android.settings;
+
+import android.support.annotation.NonNull;
+
+import com.b_designworks.android.BaseActivity;
+import com.b_designworks.android.R;
+import com.b_designworks.android.utils.UiInfo;
+
+/**
+ * Created by Ilya Eremin on 04.08.2016.
+ */
+public class PushNotificationsSettingsScreen extends BaseActivity {
+
+    @NonNull @Override public UiInfo getUiInfo() {
+        return new UiInfo(R.layout.screen_push_notifications_settings)
+            .enableBackButton()
+            .setTitleRes(R.string.title_push_notifications);
+    }
+
+}
