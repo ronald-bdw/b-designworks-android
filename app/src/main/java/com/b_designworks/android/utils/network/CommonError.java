1225a66db112eced1742e1f49fb223277fc6d6f0
Илья Еремин <partissanei@gmail.com>
2016-08-15 15:10:52 +0300

    add common error model

---
+++ app/src/main/java/com/b_designworks/android/utils/network/CommonError.java
@@ -0,0 +1,13 @@
+package com.b_designworks.android.utils.network;
+
+/**
+ * Created by Ilya Eremin on 12.08.2016.
+ */
+public class CommonError {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+}
