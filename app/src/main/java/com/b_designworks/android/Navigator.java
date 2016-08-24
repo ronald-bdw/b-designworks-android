fbe26bef458bdca781277ccb3f86dfe076f138a7
Илья Еремин <partissanei@gmail.com>
2016-08-24 16:16:05 +0300

    refactoring

--- app/src/main/java/com/b_designworks/android/Navigator.java
+++ app/src/main/java/com/b_designworks/android/Navigator.java
@@ -5,20 +5,30 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import com.b_designworks.android.chat.ChatScreen;
+import com.b_designworks.android.login.EnterPhoneScreen;
+import com.b_designworks.android.login.RegistrationScreen;
 import com.b_designworks.android.login.SelectProviderScreen;
+import com.b_designworks.android.login.VerifyScreen;
+import com.b_designworks.android.login.WelcomeScreen;
 import com.b_designworks.android.profile.EditProfileScreen;
 import com.b_designworks.android.profile.ProfileScreen;
-import com.b_designworks.android.login.RegistrationScreen;
 import com.b_designworks.android.settings.PushNotificationsSettingsScreen;
 import com.b_designworks.android.settings.SettingsScreen;
 import com.b_designworks.android.sync.SyncScreen;
 import com.b_designworks.android.trial.TrialScreen;
-import com.b_designworks.android.verification.VerifyScreen;
 
 /**
  * Created by Ilya Eremin on 03.08.2016.
  */
 public class Navigator {
+
+    public static void welcome(@NonNull Context context) {
+        context.startActivity(clearStack(new Intent(context, WelcomeScreen.class)));
+    }
+
+    public static void enterPhone(@NonNull Context context) {
+        context.startActivity(new Intent(context, EnterPhoneScreen.class));
+    }
 
     public static void trialPage(@NonNull Context context) {
         context.startActivity(new Intent(context, TrialScreen.class));
@@ -28,8 +38,16 @@ public class Navigator {
         context.startActivity(new Intent(context, VerifyScreen.class));
     }
 
+    public static void verification(@NonNull Context context, String phone) {
+        context.startActivity(VerifyScreen.createIntent(context, phone));
+    }
+
     public static void chat(@NonNull Context context) {
-        context.startActivity(new Intent(context, ChatScreen.class));
+        context.startActivity(clearStack(new Intent(context, ChatScreen.class)));
+    }
+
+    private static Intent clearStack(Intent intent) {
+        return intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
     }
 
     public static void profile(@NonNull Context context) {
@@ -52,20 +70,24 @@ public class Navigator {
         context.startActivity(new Intent(context, EditProfileScreen.class));
     }
 
-    public static void signUp(@NonNull Context context) {
-        context.startActivity(new Intent(context, RegistrationScreen.class));
-    }
-
     public static void aboutUs(@NonNull Context context) {
         context.startActivity(new Intent(context, AboutUsScreen.class));
     }
 
-    public static void registration(@NonNull Context context, @NonNull String phone, @NonNull String phoneCodeId) {
-        Intent intent  = RegistrationScreen.createIntent(context, phone, phoneCodeId);
-        context.startActivity(intent);
-    }
-
     public static void selectProvider(@NonNull Context context) {
         context.startActivity(new Intent(context, SelectProviderScreen.class));
+    }
+
+    public static void registration(@NonNull Context context, @NonNull String code) {
+        context.startActivity(RegistrationScreen.createIntent(context, code));
+    }
+
+    public static void registration(@NonNull Context context) {
+        context.startActivity(new Intent(context, RegistrationScreen.class));
+    }
+
+    public static void verifyAndReturnCode(@NonNull BaseActivity activity,
+                                           @NonNull String phone) {
+        activity.startActivityForResult(VerifyScreen.createIntent(activity, phone), RegistrationScreen.RESULT_KEY_FOR_VERIFYING);
     }
 }
--- app/src/main/java/com/b_designworks/android/login/EnterPhoneScreen.java
+++ app/src/main/java/com/b_designworks/android/login/EnterPhoneScreen.java
@@ -1,35 +1,35 @@
 package com.b_designworks.android.login;
 
 import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.b_designworks.android.BaseActivity;
 import com.b_designworks.android.BuildConfig;
-import com.b_designworks.android.DI;
 import com.b_designworks.android.Navigator;
 import com.b_designworks.android.R;
-import com.b_designworks.android.utils.Rxs;
-import com.b_designworks.android.utils.network.ErrorUtils;
 import com.b_designworks.android.utils.ui.TextViews;
 import com.b_designworks.android.utils.ui.UiInfo;
 
 import butterknife.Bind;
 import butterknife.OnClick;
-import rx.Subscription;
 
 /**
  * Created by Ilya Eremin on 03.08.2016.
  */
 public class EnterPhoneScreen extends BaseActivity {
 
-    @Bind(R.id.phone) EditText uiPhone;
+    @Bind(R.id.phone)  EditText uiPhone;
+    @Bind(R.id.submit) Button   uiSubmit;
 
     @NonNull @Override public UiInfo getUiInfo() {
-        return new UiInfo(R.layout.screen_enter_phone);
+        return new UiInfo(R.layout.screen_enter_phone)
+            .setTitleRes(R.string.title_verification)
+            .enableBackButton();
     }
 
     @SuppressLint("SetTextI18n") @Override protected void onCreate(@Nullable Bundle savedState) {
@@ -39,34 +39,14 @@ public class EnterPhoneScreen extends BaseActivity {
         }
     }
 
-    @Nullable ProgressDialog progresDialog;
-    @Nullable Subscription   sendingSubscription;
-
-    @OnClick(R.id.next) void onNextClick() {
-        if(sendingSubscription != null) return;
-        progresDialog = ProgressDialog.show(context(), getString(R.string.loading), getString(R.string.loading_sending_code));
-        sendingSubscription = DI.getInstance().getApi().getCode(TextViews.textOf(uiPhone))
-            .compose(Rxs.doInBackgroundDeliverToUI())
-            .doOnEach(i -> {
-                if (progresDialog != null) {
-                    progresDialog.dismiss();
-                }
-                sendingSubscription = null;
-            })
-            .subscribe(result -> {
-                if (result.isPhoneRegistered()) {
-                    Navigator.selectProvider(context());
-                } else {
-                    Navigator.registration(context(), TextViews.textOf(uiPhone), result.getPhoneCodeId());
-                }
-            }, ErrorUtils.onError());
-    }
-
-    @Override protected void onStop() {
-        if (sendingSubscription != null && !sendingSubscription.isUnsubscribed()) {
-            sendingSubscription.unsubscribe();
+    @OnClick(R.id.submit) void onSubmitClick() {
+        String phone = TextViews.textOf(uiPhone);
+        if (!TextUtils.isEmpty(phone)) {
+            // TODO what if phone number has incorrect format
+            Navigator.verification(context(), phone);
+        } else {
+            uiPhone.setError(getString(R.string.registration_error_fill_phone));
         }
-        super.onStop();
     }
 
 }
