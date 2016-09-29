c9e85b9ee51cddbbe3f6d02e288ceccaca097d7a
Ilya Eremin <ilya.eremin@flatstack.com>
2016-09-29 16:39:15 +0300

    send changes on tour profile screen

--- app/src/main/java/com/b_designworks/android/tour_app/TourScreen1Profile.java
+++ app/src/main/java/com/b_designworks/android/tour_app/TourScreen1Profile.java
@@ -1,23 +1,30 @@
 package com.b_designworks.android.tour_app;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.b_designworks.android.BaseActivity;
 import com.b_designworks.android.Navigator;
 import com.b_designworks.android.R;
 import com.b_designworks.android.UserInteractor;
 import com.b_designworks.android.login.models.User;
+import com.b_designworks.android.utils.Rxs;
 import com.b_designworks.android.utils.di.Injector;
+import com.b_designworks.android.utils.network.ErrorUtils;
 import com.b_designworks.android.utils.ui.UiInfo;
 
 import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.OnClick;
+import rx.Subscription;
+
+import static com.b_designworks.android.utils.ui.TextViews.textOf;
 
 /**
  * Created by Ilya Eremin on 9/28/16.
@@ -32,8 +39,8 @@ public class TourScreen1Profile extends BaseActivity {
     @Inject UserInteractor userInteractor;
 
     @NonNull @Override public UiInfo getUiInfo() {
-        return new UiInfo(R.layout.screen_tour_1)
-            .setTitleRes(R.string.title_tour_1)
+        return new UiInfo(R.layout.screen_tour_profile)
+            .setTitleRes(R.string.title_tour_1_page)
             .setMenuRes(R.menu.menu_with_next_btn);
     }
 
@@ -46,21 +53,61 @@ public class TourScreen1Profile extends BaseActivity {
         uiEmail.setText(user.getEmail());
     }
 
+    @Nullable private Subscription   updateProfileSubs;
+    @Nullable private ProgressDialog updateProfileProgress;
+
     @Override public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.next) {
-            Navigator.tour2(context());
+            if (updateProfileSubs != null) return true;
+            sendInfoAndMoveToNextScreen();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @OnClick(R.id.next) void onSubmitClick() {
-        // TODO update profile before redirect
-        Navigator.tour2(context());
+    private void sendInfoAndMoveToNextScreen() {
+        if (fieldsChanged()) {
+            showUpdateProfileProgress();
+            updateProfileSubs = userInteractor.updateUser(textOf(uiFirstName), textOf(uiLastName), textOf(uiEmail))
+                .doOnTerminate(() -> {
+                    updateProfileSubs = null;
+                    hideProgress();
+                })
+                .compose(Rxs.doInBackgroundDeliverToUI())
+                .subscribe(result -> {
+                    Toast.makeText(context(), R.string.edit_profile_profile_updated, Toast.LENGTH_SHORT).show();
+                    Navigator.tourUploadAvatar(context());
+                }, ErrorUtils.handle(context()));
+        } else {
+            Navigator.tourUploadAvatar(context());
+        }
     }
 
-    @OnClick(R.id.skip) void onSkipClick() {
-        Navigator.tour2(context());
+    private void hideProgress() {
+        if (updateProfileProgress != null) {
+            updateProfileProgress.dismiss();
+        }
+    }
+
+    private void showUpdateProfileProgress() {
+        hideProgress();
+        updateProfileProgress = ProgressDialog.show(context(), getString(R.string.loading), getString(R.string.loading_user_info));
+        updateProfileProgress.setOnDismissListener(dialog -> {
+            if (updateProfileSubs != null && !updateProfileSubs.isUnsubscribed()) {
+                updateProfileSubs.unsubscribe();
+            }
+        });
+    }
+
+    private boolean fieldsChanged() {
+        User user = userInteractor.getUser();
+        return !textOf(uiFirstName).equals(user.getFirstName()) ||
+            !textOf(uiLastName).equals(user.getLastName()) ||
+            !textOf(uiEmail).equals(user.getEmail());
+    }
+
+    @OnClick(R.id.next) void onSubmitClick() {
+        sendInfoAndMoveToNextScreen();
     }
 
 }
