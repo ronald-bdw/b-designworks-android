6a0e69defc1ca747c9563410fd752aec364de042
Ilya Eremin <ilya.eremin@flatstack.com>
2016-10-11 19:22:27 +0300

    customize smooch conversationActivity for our needs

--- app/src/main/java/com/b_designworks/android/chat/ChatScreen.java
+++ app/src/main/java/com/b_designworks/android/chat/ChatScreen.java
@@ -1,49 +1,52 @@
 package com.b_designworks.android.chat;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-import com.b_designworks.android.BaseActivity;
 import com.b_designworks.android.R;
 import com.b_designworks.android.UserInteractor;
+import com.b_designworks.android.utils.AndroidUtils;
 import com.b_designworks.android.utils.Bus;
 import com.b_designworks.android.utils.Keyboard;
 import com.b_designworks.android.utils.di.Injector;
-import com.b_designworks.android.utils.ui.UiInfo;
 
 import org.greenrobot.eventbus.Subscribe;
 
 import javax.inject.Inject;
 
-import butterknife.Bind;
 import io.smooch.core.Smooch;
 import io.smooch.core.User;
-import io.smooch.ui.fragment.ConversationFragment;
+import io.smooch.ui.ConversationActivity;
 
 /**
  * Created by Ilya Eremin on 04.08.2016.
  */
-public class ChatScreen extends BaseActivity {
-
-    private static final String TAG = "ChatScreen";
+public class ChatScreen extends ConversationActivity {
 
     @Inject UserInteractor userInteractor;
 
-    @Bind(R.id.drawer) DrawerLayout uiDrawer;
-
-    @NonNull @Override public UiInfo getUiInfo() {
-        return new UiInfo(R.layout.screen_chat).setMenuRes(R.menu.chat);
-    }
+    private DrawerLayout uiDrawer;
 
     @Override protected void onCreate(@Nullable Bundle savedState) {
         super.onCreate(savedState);
         Injector.inject(this);
+
+        addRightPanel();
+
         if (savedState == null) {
             if (userInteractor.firstVisitAfterLogin()) {
                 Smooch.logout();
@@ -56,10 +59,8 @@ public class ChatScreen extends BaseActivity {
             User.getCurrentUser().setFirstName(user.getFirstName());
             User.getCurrentUser().setLastName(user.getId());
 
-            Log.d(TAG, "Current user id  is: " + userInteractor.getUserId());
             getSupportFragmentManager().beginTransaction()
-                .replace(R.id.chat_container, new ConversationFragment())
-                .replace(R.id.side_panel_container, new ChatSidePanelFragment())
+                .replace(R.id.chat_right_panel, new ChatSidePanelFragment())
                 .commit();
         }
         uiDrawer.addDrawerListener(new DrawerLayout.DrawerListener() {
@@ -82,6 +83,36 @@ public class ChatScreen extends BaseActivity {
 
     }
 
+    private void addRightPanel() {
+        uiDrawer = new DrawerLayout(this);
+
+        final FrameLayout rightSidePanelCountainer = new FrameLayout(this);
+        final FrameLayout chatContainer = new FrameLayout(this);
+
+        rightSidePanelCountainer.setId(R.id.chat_right_panel);
+        DrawerLayout.LayoutParams lp = new DrawerLayout.LayoutParams(AndroidUtils.dp(240), LinearLayout.LayoutParams.MATCH_PARENT);
+        lp.gravity = Gravity.END;
+        rightSidePanelCountainer.setLayoutParams(lp);
+
+        ViewGroup contentView = ((ViewGroup) findViewById(android.R.id.content));
+        ViewGroup rootView = (ViewGroup) contentView.getChildAt(0);
+        contentView.removeView(rootView);
+        rootView.setBackgroundColor(Color.WHITE);
+        chatContainer.addView(rootView, new DrawerLayout.LayoutParams(DrawerLayout.LayoutParams.MATCH_PARENT, DrawerLayout.LayoutParams.MATCH_PARENT));
+        LayoutInflater.from(this).inflate(R.layout.layout_toolbar, chatContainer);
+        Toolbar uiToolbar = (Toolbar) chatContainer.findViewById(R.id.toolbar);
+        setSupportActionBar(uiToolbar);
+        uiDrawer.addView(chatContainer, new DrawerLayout.LayoutParams(DrawerLayout.LayoutParams.MATCH_PARENT, DrawerLayout.LayoutParams.MATCH_PARENT));
+        uiDrawer.addView(rightSidePanelCountainer);
+        contentView.addView(uiDrawer);
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chat, menu);
+        return true;
+    }
+
     @Override public void onBackPressed() {
         if (uiDrawer.isDrawerOpen(GravityCompat.END)) {
             closeDrawer();
@@ -90,8 +121,22 @@ public class ChatScreen extends BaseActivity {
         }
     }
 
-    @Override protected void onResume() {
+    @Override public void onResume() {
         super.onResume();
+        // the only way to customize smooch UI
+        findViewById(R.id.scrollView).setBackgroundResource(R.drawable.white_with_round_corners);
+        View uiInputText = findViewById(R.id.Smooch_inputText);
+        uiInputText.setBackgroundColor(Color.WHITE);
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) uiInputText.getLayoutParams();
+        layoutParams.topMargin = AndroidUtils.dp(8);
+        layoutParams.bottomMargin = AndroidUtils.dp(8);
+        layoutParams.leftMargin = AndroidUtils.dp(8);
+
+        layoutParams.addRule(RelativeLayout.RIGHT_OF, 0);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+
+        ((View) uiInputText.getParent()).setBackgroundColor(0xFFECF3FA);
+        findViewById(R.id.Smooch_btnCamera).setVisibility(View.GONE);
         Bus.subscribe(this);
     }
 
@@ -99,7 +144,7 @@ public class ChatScreen extends BaseActivity {
         closeDrawer();
     }
 
-    @Override protected void onPause() {
+    @Override public void onPause() {
         Bus.unsubscribe(this);
         super.onPause();
     }
