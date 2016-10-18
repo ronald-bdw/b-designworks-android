b8da79a5c0d5ab7e2f808648bcebe0d1c4e8846d
AlmazIskhakovFlatstack <almaz.iskhakov@flatstack.com>
2016-10-18 11:34:49 +0300

    add doOnError and doOnTerminate to calling

--- app/src/main/java/com/b_designworks/android/profile/ProfileScreen.java
+++ app/src/main/java/com/b_designworks/android/profile/ProfileScreen.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.b_designworks.android.BaseActivity;
 import com.b_designworks.android.Navigator;
@@ -18,6 +19,7 @@ import com.b_designworks.android.utils.ImageLoader;
 import com.b_designworks.android.utils.Logger;
 import com.b_designworks.android.utils.Rxs;
 import com.b_designworks.android.utils.di.Injector;
+import com.b_designworks.android.utils.network.ErrorUtils;
 import com.b_designworks.android.utils.ui.UiInfo;
 
 import javax.inject.Inject;
@@ -27,7 +29,7 @@ import butterknife.Bind;
 /**
  * Created by Ilya Eremin on 04.08.2016.
  */
-public class ProfileScreen extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener{
+public class ProfileScreen extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @Inject UserInteractor userInteractor;
 
@@ -35,11 +37,11 @@ public class ProfileScreen extends BaseActivity implements SwipeRefreshLayout.On
         return new UiInfo(R.layout.screen_profile).enableBackButton().setMenuRes(R.menu.profile);
     }
 
-    @Bind(R.id.avatar)            ImageView uiAvatar;
-    @Bind(R.id.email)             TextView  uiEmail;
-    @Bind(R.id.phone)             TextView  uiPhone;
-    @Bind(R.id.current_full_name) TextView  uiCurrentFullName;
-    @Bind(R.id.current_email)     TextView  uiCurrentEmail;
+    @Bind(R.id.avatar)            ImageView          uiAvatar;
+    @Bind(R.id.email)             TextView           uiEmail;
+    @Bind(R.id.phone)             TextView           uiPhone;
+    @Bind(R.id.current_full_name) TextView           uiCurrentFullName;
+    @Bind(R.id.current_email)     TextView           uiCurrentEmail;
     @Bind(R.id.swipe_to_refresh)  SwipeRefreshLayout uiSwipeRefreshLayout;
 
     @Override protected void onCreate(@Nullable Bundle savedState) {
@@ -65,26 +67,24 @@ public class ProfileScreen extends BaseActivity implements SwipeRefreshLayout.On
         uiSwipeRefreshLayout.setRefreshing(true);
         userInteractor.updateUserProfile()
             .compose(Rxs.doInBackgroundDeliverToUI())
-            .subscribe(this::updateUserProfile, Logger::e);
+            .doOnTerminate(() -> uiSwipeRefreshLayout.setRefreshing(false))
+            .doOnError((e) -> ErrorUtils.handle(context(), e))
+            .subscribe(this::showUser, Logger::e);
     }
 
-    private void tuneSwipeRefreshLayout(){
-        uiSwipeRefreshLayout.setRefreshing(false);
+    private void tuneSwipeRefreshLayout() {
         uiSwipeRefreshLayout.setOnRefreshListener(this);
         uiSwipeRefreshLayout.setColorSchemeResources(R.color.app_accent);
     }
 
-    private void updateUserProfile(User user){
-        showUser(user);
-        uiSwipeRefreshLayout.setRefreshing(false);
-    }
-
-    private void showUser(User user){
-        ImageLoader.load(this, uiAvatar, user.getAvatar().getOriginal());
-        uiCurrentFullName.setText(getString(R.string.edit_profile_name_surname_pattern, user.getFirstName(), user.getLastName()));
-        uiEmail.setText(user.getEmail());
-        uiCurrentEmail.setText(user.getEmail());
-        uiPhone.setText(user.getPhoneNumber());
-        setTitle(user.getFirstName());
+    private void showUser(User user) {
+        if (context() != null) {
+            ImageLoader.load(this, uiAvatar, user.getAvatar().getOriginal());
+            uiCurrentFullName.setText(getString(R.string.edit_profile_name_surname_pattern, user.getFirstName(), user.getLastName()));
+            uiEmail.setText(user.getEmail());
+            uiCurrentEmail.setText(user.getEmail());
+            uiPhone.setText(user.getPhoneNumber());
+            setTitle(user.getFirstName());
+        }
     }
 }
