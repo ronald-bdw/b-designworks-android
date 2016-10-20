4912ec660523474675ec9be91646ace310a85b52
Ilya Eremin <ilya.eremin@flatstack.com>
2016-10-20 19:31:08 +0300

    fix tests

--- app/src/main/java/com/b_designworks/android/utils/Bus.java
+++ app/src/main/java/com/b_designworks/android/utils/Bus.java
@@ -7,27 +7,36 @@ import org.greenrobot.eventbus.EventBus;
  */
 public class Bus {
     public static final boolean STICKY = true;
+    public static boolean DISABLE_FOR_TEST;
 
     /**
      * to speed up eventBus use code generation.
      *
      * @see <a href="http://greenrobot.org/eventbus/documentation/subscriber-index/">GreenRobot Eventbus docs</a>
      */
-    private static final EventBus bus = EventBus.builder()
-        .build();
+    private static EventBus bus;
 
-    private Bus() {
-    }
+    private Bus() {}
 
     public static void subscribe(Object object) {
+        initialize();
         bus.register(object);
     }
 
+    private static void initialize() {
+        if (bus == null) {
+            bus = EventBus.builder().build();
+        }
+    }
+
     public static void unsubscribe(Object object) {
+        initialize();
         bus.unregister(object);
     }
 
     public static void event(Object object, boolean... sticky) {
+        if(DISABLE_FOR_TEST) return;
+        initialize();
         if (sticky != null && sticky.length > 0) {
             bus.postSticky(object);
         } else {
