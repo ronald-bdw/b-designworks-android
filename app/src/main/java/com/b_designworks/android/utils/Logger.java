a59afa717214b8c96dca845330d2654cfb2a5099
Ilya Eremin <ilya.eremin@flatstack.com>
2016-10-20 11:58:42 +0300

    add info and debug toast for logger

--- app/src/main/java/com/b_designworks/android/utils/Logger.java
+++ app/src/main/java/com/b_designworks/android/utils/Logger.java
@@ -1,7 +1,10 @@
 package com.b_designworks.android.utils;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.design.BuildConfig;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Ilya Eremin on 10/13/16.
@@ -13,5 +16,15 @@ public class Logger {
 
     public static void e(@NonNull Throwable error) {
         Log.e(TAG, error.getMessage(), error);
+    }
+
+    public static void i(String msg) {
+        Log.i(TAG, msg);
+    }
+
+    public static void dToast(@NonNull String msg, @NonNull Context context) {
+        if (BuildConfig.DEBUG && !BuildConfig.FLAVOR.equals("production")) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }
     }
 }
