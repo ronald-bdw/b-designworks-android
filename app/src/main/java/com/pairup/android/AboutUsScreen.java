dedcb278b747a786f0e05b0f7e62ce0e190c13ab
AlmazIskhakovFlatstack <almaz.iskhakov@flatstack.com>
2016-11-22 19:35:23 +0300

    Refactored resources

--- app/src/main/java/com/pairup/android/AboutUsScreen.java
+++ app/src/main/java/com/pairup/android/AboutUsScreen.java
@@ -4,11 +4,19 @@ import android.support.annotation.NonNull;
 
 import com.pairup.android.utils.ui.UiInfo;
 
+import butterknife.OnClick;
+
 /**
  * Created by Ilya Eremin on 09.08.2016.
  */
 public class AboutUsScreen extends BaseActivity {
+
+    private static final String WEBLINK = "http://pairup.im";
     @NonNull @Override public UiInfo getUiInfo() {
         return new UiInfo(R.layout.screen_about_us).setTitleRes(R.string.title_about_us).enableBackButton();
+    }
+
+    @OnClick(R.id.website_link) void onClicKWeblink(){
+        Navigator.openUrl(this, WEBLINK);
     }
 }
