1abb2ddf0b336b9e7d95e2e3ca30e99cbbe5ef6e
AlmazIskhakovFlatstack <almaz.iskhakov@flatstack.com>
2016-12-13 15:31:54 +0300

    Add blocking screen turning

--- app/src/main/java/com/pairup/android/BaseActivity.java
+++ app/src/main/java/com/pairup/android/BaseActivity.java
@@ -2,6 +2,7 @@ package com.pairup.android;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -29,6 +30,8 @@ public abstract class BaseActivity extends RxAppCompatActivity {
     public abstract @NonNull UiInfo getUiInfo();
 
     @Override protected void onCreate(@Nullable Bundle savedState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         super.onCreate(savedState);
         Dart.inject(this);
         setContentView(getUiInfo().getLayoutRes());
