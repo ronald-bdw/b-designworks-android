be394f4d06dc1c92fa24334d2a5167511183a223
sergey.klymenko <sergey.klymenko@flatstack.com>
2017-01-06 18:59:51 +0200

    refactoring

--- app/src/main/java/com/pairup/android/utils/di/Injector.java
+++ app/src/main/java/com/pairup/android/utils/di/Injector.java
@@ -85,6 +85,8 @@ public class Injector {
     }
 
     public static void inject(@NonNull SelectProviderScreen selectProviderScreen) {
-        ((App) selectProviderScreen.getApplicationContext()).getAppComponent().inject(selectProviderScreen);
+        ((App) selectProviderScreen.getApplicationContext())
+                .getAppComponent()
+                .inject(selectProviderScreen);
     }
 }
