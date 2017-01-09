0661d87bf9e60b76c14bcc125da954af773be0b3
sergey.klymenko <sergey.klymenko@flatstack.com>
2017-01-09 14:57:18 +0200

    refactoring

--- app/src/main/java/com/pairup/android/login/SelectProviderPresenter.java
+++ app/src/main/java/com/pairup/android/login/SelectProviderPresenter.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import com.pairup.android.UserInteractor;
 import com.pairup.android.login.models.Provider;
 import com.pairup.android.utils.Rxs;
-import com.pairup.android.utils.network.ErrorUtils;
 
 import java.util.ArrayList;
 import java.util.List;
--- app/src/main/java/com/pairup/android/login/VerifyScreen.java
+++ app/src/main/java/com/pairup/android/login/VerifyScreen.java
@@ -4,11 +4,9 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.ImageView;
 
 import com.pairup.android.BaseActivity;
 import com.pairup.android.Navigator;
