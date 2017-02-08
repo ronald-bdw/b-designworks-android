3abd950b56ef7012bf1aead3e4f0103b6350224b
AlmazIskhakovFlatstack <almaz.iskhakov@flatstack.com>
2017-02-08 15:21:25 +0300

    Replace subs with new prices

--- app/src/main/java/com/pairup/android/subscription/Subscription.java
+++ app/src/main/java/com/pairup/android/subscription/Subscription.java
@@ -6,9 +6,9 @@ package com.pairup.android.subscription;
 
 public enum Subscription {
 
-    THREE_MONTH_SUBSCRIPTION_ID("three_month_subscription_v1", "habit_starter", 3),
-    SIX_MONTH_SUBSCRIPTION_ID("six_month_subscription_v1", "habit_stabliser", 6),
-    ONE_YEAR_SUBSCRIPTION_ID("one_year_subscription_v1", "habit_master", 12);
+    THREE_MONTH_SUBSCRIPTION_ID("three_month_subscription_v3", "habit_starter", 3),
+    SIX_MONTH_SUBSCRIPTION_ID("six_month_subscription_v3", "habit_stabiliser", 6),
+    ONE_YEAR_SUBSCRIPTION_ID("one_year_subscription_v3", "habit_master", 12);
 
     private String planId;
     private String planName;
