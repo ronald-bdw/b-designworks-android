ae9504b2dd77afd31df15dbb589673f15cf36ee9
AlmazIskhakovFlatstack <almaz.iskhakov@flatstack.com>
2017-02-10 19:15:01 +0300

    Add subscription dialog class

---
+++ app/src/main/java/com/pairup/android/subscription/SubscriptionDialog.java
@@ -0,0 +1,43 @@
+package com.pairup.android.subscription;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.pairup.android.R;
+import com.pairup.android.utils.Bus;
+import com.pairup.android.utils.ui.BaseDialogFragment;
+import com.pairup.android.utils.ui.UiInfo;
+
+import butterknife.OnClick;
+import rx.functions.Action1;
+
+/**
+ * Created by almaziskhakov on 10/02/2017.
+ */
+
+public class SubscriptionDialog extends BaseDialogFragment {
+
+    public static void show(FragmentActivity activity) {
+        show(new SubscriptionDialog(), activity);
+    }
+
+    @Override protected UiInfo getUiInfo() {
+        return new UiInfo(R.layout.dialog_subscription);
+    }
+
+    @OnClick(R.id.starter) void onStartClick() {
+        Bus.event(SubscriptionDialogItemTabEvent.STARTER);
+        getDialog().dismiss();
+    }
+
+    @OnClick(R.id.stabilizer) void onStabilizerClick() {
+        Bus.event(SubscriptionDialogItemTabEvent.STABILIZER);
+        getDialog().dismiss();
+    }
+
+    @OnClick(R.id.master) void onMasterClick() {
+        Bus.event(SubscriptionDialogItemTabEvent.MASTER);
+        getDialog().dismiss();
+    }
+
+}
