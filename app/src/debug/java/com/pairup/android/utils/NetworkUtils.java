c33aca73b294406a09150cea51d6c3eef6b62d69
AlmazIskhakovFlatstack <almaz.iskhakov@flatstack.com>
2017-03-23 17:40:48 +0300

    Add debug and release separation on level 'main'

---
+++ app/src/debug/java/com/pairup/android/utils/NetworkUtils.java
@@ -0,0 +1,27 @@
+package com.pairup.android.utils;
+
+import android.content.Context;
+
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+/**
+ * Created by almaziskhakov on 23/03/2017.
+ */
+
+public class NetworkUtils {
+
+    public static void initStetho(Context context) {
+        Stetho.initializeWithDefaults(context);
+    }
+
+    public static void addInterceptorsToClientBuilder(OkHttpClient.Builder httpClientBuilder) {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClientBuilder.addInterceptor(interceptor);
+        httpClientBuilder.addNetworkInterceptor(new StethoInterceptor());
+    }
+}
---
+++ app/src/release/java/com/pairup/android/utils/NetworkUtils.java
@@ -0,0 +1,20 @@
+package com.pairup.android.utils;
+
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by almaziskhakov on 23/03/2017.
+ */
+
+public class NetworkUtils {
+
+    public static void initStetho(Context context){
+        //do nothing
+    }
+
+    public static void addInterceptorsToClientBuilder(OkHttpClient.Builder httpClientBuilder) {
+        //add nothing
+    }
+}
